980b5a43be296e77fbe217fa1396569c880bc3fe
Kacper Kuźniak <kacperkuzniak@pc-2736.home>
2018-07-01 16:27:12 +0200

    Initial Commit

---
+++ NickByNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NickByNumber
+//
+//  Created by Kacper Kuźniak on 01/07/2018.
+//  Copyright © 2018 Kacper Kuźniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
